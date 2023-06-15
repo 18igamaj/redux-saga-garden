4cb4753bd958962a27be9d179762a113d4a31004
Majid Igale <majidigale18@gmail.com>
2023-06-14 20:17:17 -0500

    added saga functionality

--- src/components/PlantList/PlantList.js
+++ src/components/PlantList/PlantList.js
@@ -5,19 +5,38 @@ import { useDispatch, useSelector} from 'react-redux';
 function PlantList() {
     const dispatch = useDispatch();
 
-    const reduxState = useSelector(store => store);
+    const reduxState = useSelector(store => store.plantList);
 
     useEffect(() => {
         console.log('component did mount');
         // dispatch an action to request the plantList from the API
+        dispatch({type:'FETCH_PLANTS'})
     }, []); 
+
+    const removePlant = (id) => {
+        dispatch({
+            type:'REMOVE_PLANT',
+            payload: id
+        })
+    }
 
     return (
         <div>
             <h3>This is the plant list</h3>
-            <pre>{JSON.stringify(reduxState)}</pre>
+            {/* <pre>{JSON.stringify(reduxState)}</pre> */}
+        
+            {reduxState.map((plant) => (
+                <div key={plant.id}>
+                <p > {plant.name} </p>
+                <button onClick={() => removePlant(plant.id)}>Delete</button>
+                </div>
+                
+            )) 
+            }
         </div>
     );
+    //{gifs.map((gif, i) => (
+      //  <img key={i} src={gif.images.original.url}
 }
 
 export default PlantList;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,85 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-
+import logger from 'redux-logger'
+import {takeLatest, put} from 'redux-saga/effects'
+import createSagaMiddleware from 'redux-saga'
+import axios from 'axios'
 import App from './App';
 
 // this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' }
+// ];
 
-const plantList = (state = startingPlantArray, action) => {
+const plantList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+      return action.payload 
     default:
       return state;
   }
 };
 
+function* watcherSaga() {
+  yield takeLatest('FETCH_PLANTS', fetchSaga)
+  yield takeLatest('POST_PLANT', postSaga)
+  yield takeLatest('REMOVE_PLANT', removeSaga)
+}
+
+function* postSaga(action) {
+  try {
+    console.log('Is our action Post showing up?', action)
+
+    yield axios.post('/api/plant', action.payload)
+
+    yield put({
+      type: 'FETCH_PLANTS'
+    })
+  } catch(error){
+    console.log('Oh no our Post Saga', error)
+  }
+}
+
+function* fetchSaga(){
+
+  try {
+    const plants = yield axios.get('/api/plant')
+    yield put({
+      type: 'ADD_PLANT',
+      payload: plants.data
+    })
+  } catch(err){
+
+  }
+}
+
+function* removeSaga(action){
+try{
+  yield axios.delete(`/api/plant/${action.payload}`)
+  yield put({
+    type:'FETCH_PLANTS'
+  })
+} catch(error) {
+  console.log('Error with DELETE', error)
+}
+
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware(sagaMiddleware, logger)
 );
+
+
+
+
+sagaMiddleware.run(watcherSaga)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
